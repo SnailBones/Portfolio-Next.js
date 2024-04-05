5114be268a89dbc8fff5529b6172b25fc59a5db9
Aidan Hendrickson <aidhendrickson@gmail.com>
2024-04-05 12:07:49 -0600

    Updating base path

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withMDX = require("@next/mdx")();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+    basePath: "/demo",
 };
 
 module.exports = withMDX(nextConfig);
